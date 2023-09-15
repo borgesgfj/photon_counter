c95efc21c95f55a9cdcc240c00ca071b970f9843
Gilberto Borges <borgesgfj@gmail.com>
2023-09-15 14:25:57 +0200

    create entrypoint

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from remote_connector_dao.plot_data_test import test_run
+
+
+test_run()
--- src/remote_connector_dao/plot_data_test.py
+++ src/remote_connector_dao/plot_data_test.py
@@ -3,30 +3,30 @@ from remote_connector_dao.remote_connector_dao import connect_to_server
 import numpy as np
 import matplotlib.pyplot as plt
 
+def test_run():
+  tagger, TimeTagger = connect_to_server("192.168.1.162:23000")
 
-tagger, TimeTagger = connect_to_server("192.168.1.162:23000")
+  tagger.setTriggerLevel(1, 0.09)
+  tagger.setTriggerLevel(2, 0.09)
 
-tagger.setTriggerLevel(1, 0.09)
-tagger.setTriggerLevel(2, 0.09)
+  hist = TimeTagger.Histogram(
+      tagger, start_channel=2, click_channel=1, binwidth=100, n_bins=1000
+  )
 
-hist = TimeTagger.Histogram(
-    tagger, start_channel=2, click_channel=1, binwidth=100, n_bins=1000
-)
+  hist.startFor(int(10e12), clear=True)
 
-hist.startFor(int(10e12), clear=True)
+  while hist.isRunning():
+      time.sleep(0.3)
 
-while hist.isRunning():
-    time.sleep(0.3)
+  data = np.array(hist.getData())
+  print(np.max(data))
+  # trigger_counts = corr.getCounts()
+  plt.figure(figsize=(3, 2))
+  plt.plot(data)
+  plt.show()
 
-data = np.array(hist.getData())
-print(np.max(data))
-# trigger_counts = corr.getCounts()
-plt.figure(figsize=(3, 2))
-plt.plot(data)
-plt.show()
-
-# Cleanup
-TimeTagger.freeTimeTagger(tagger)
-del hist
-del tagger
-del TimeTagger
+  # Cleanup
+  TimeTagger.freeTimeTagger(tagger)
+  del hist
+  del tagger
+  del TimeTagger
