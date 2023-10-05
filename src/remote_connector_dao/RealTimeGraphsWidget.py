b8860d6c3787135babd793f0c9dd398ac62c708b
Gilberto Borges <borgesgfj@gmail.com>
2023-10-05 16:59:41 +0200

    [refactor] refactor RealTimeGraphsWidget

--- src/remote_connector_dao/RealTimeGraphsWidget.py
+++ src/remote_connector_dao/RealTimeGraphsWidget.py
@@ -27,35 +27,49 @@ class RealTimeGraphsWidget(QWidget):
         self.channel2_counts = self.signal_counter.channel2
         self.coincidences = self.signal_counter.coincidences
 
+        self._set_single_counts_graph_lines()
+        self._set_coincidences_graph_lines()
+
+        self._set_update_graph_event()
+
+        self._configure_layout()
+
+    def _update_plot_data(self):
+        self._update_counts()
+
+        self.channel1_data_line.setData(self.elapsed_time, self.channel1_counts)
+        self.channel2_data_line.setData(self.elapsed_time, self.channel2_counts)
+        self.coincidences_data_line.setData(self.elapsed_time, self.coincidences)
+
+    def _update_counts(self):
+        channel1, channel2, coincidences = get_count_rates(
+            self.timetagger_proxy, self.timetagger_controller, self.channels_list
+        )
+
+        self.signal_counter.record_measurement(channel1, channel2, coincidences)
+
+    def _set_single_counts_graph_lines(self):
         self.channel1_data_line = self.single_counts_graph_widget.plot(
             self.elapsed_time, self.channel1_counts
         )
+
         self.channel2_data_line = self.single_counts_graph_widget.plot(
             self.elapsed_time, self.channel2_counts
         )
 
+    def _set_coincidences_graph_lines(self):
         self.coincidences_data_line = self.coincidences_graph_widget.plot(
             self.elapsed_time, self.coincidences
         )
-        layout = QVBoxLayout(self)
 
+    def _set_update_graph_event(self):
         self.timer = QtCore.QTimer()
         self.timer.setInterval(GRAPH_ANIMATION_INTERVAL)
-        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.timeout.connect(self._update_plot_data)
         self.timer.start()
 
+    def _configure_layout(self):
+        layout = QVBoxLayout(self)
         layout.addWidget(self.single_counts_graph_widget)
         layout.addWidget(self.coincidences_graph_widget)
         self.setLayout(layout)
-
-    def update_plot_data(self):
-        channel1_counts, channel2_counts, coincidences_counts = get_count_rates(
-            self.timetagger_proxy, self.timetagger_controller, self.channels_list
-        )
-
-        self.signal_counter.record_measurement(
-            channel1_counts, channel2_counts, coincidences_counts
-        )
-        self.channel1_data_line.setData(self.elapsed_time, self.channel1_counts)
-        self.channel2_data_line.setData(self.elapsed_time, self.channel2_counts)
-        self.coincidences_data_line.setData(self.elapsed_time, self.coincidences)
