55049afbf7adb76fcb95086a20ce7114943ad52b
You3419 <benamarayou@gmail.com>
2024-03-18 10:51:37 +0100

    [feat] ui button

--- src/main.py
+++ src/main.py
@@ -1,7 +1,104 @@
-from remote_connector_dao.plot_data_test import test_run
-from remote_connector_dao.get_signal_tests import test_signal_run
-from remote_connector_dao.test_real_time_graph import run_real_time_graph
+import sys
+from PyQt5 import QtWidgets
+from AppController import AppController, CloseConnectionPrams
+from shared.constants.constants import TRIGGER_VOLTAGE
+from time_tagger.connection.repository import ConnectionRepository
+from time_tagger.connection.service import (
+    TimeTaggerAddressInfo,
+    ConnectionService,
+)
+from time_tagger.hardware_properties.dao import (
+    SetTriggerLevelParams,
+    TimeTaggerHardwarePropertiesDao,
+)
+from time_tagger.measurement.repository import MeasurementRepository
+from time_tagger.measurement.service import MeasurementService
+from time_tagger.builder import TimeTaggerBuilder
+from time_tagger.connection.dao import ConnectionDao
+from ui.MainWindow import MainWindow
+from time_tagger.measurement.dao import CountRateMeasurementReqParams, MeasurementDao
+"""
+    This file is a mess because I am only using this for testing.
+"""
 
-# test_run()
-run_real_time_graph()
-# test_signal_run()
+channels = [1, 2]
+connection_data = ConnectionRepository()
+
+time_tagger_hardware_properties_service = TimeTaggerHardwarePropertiesDao()
+
+connection_dao = ConnectionDao()
+
+time_tagger_network_connection_service = ConnectionService(
+    connection_data, time_tagger_hardware_properties_service, connection_dao
+)
+
+time_tagger_builder = TimeTaggerBuilder()
+
+measurement_data = MeasurementRepository()
+measurement_dao = MeasurementDao(time_tagger_builder)
+
+time_tagger_measurement_service = MeasurementService(measurement_dao,measurement_data,time_tagger_builder)
+
+app_controller = AppController(
+    time_tagger_network_connection_service=time_tagger_network_connection_service,
+    time_tagger_measurement_service=time_tagger_measurement_service,
+    time_tagger_hardware_properties=time_tagger_hardware_properties_service,
+)
+
+# The first connection should fails because there is no server on this port.
+req = app_controller.connect_to_time_taggers_network(
+    [
+        TimeTaggerAddressInfo(
+            host_address="192.168.1.162",
+            port="41101",
+            time_tagger_name="Alice_tagger",
+        )
+    ]
+)
+
+print(req, end="\n\n")
+
+print(req.connection_failed_devices, "connection failed devices", end="\n\n")
+
+tagger = req.connected_devices[0].tagger_proxy
+connected_device_name = req.connected_devices[0].time_tagger_name
+
+coincidence_virtual_channel = time_tagger_builder.build_coincidence_virtual_channel(
+    tagger, channels
+)
+
+cc_virtual_channel_numbers = coincidence_virtual_channel.getChannels()
+
+set_trigger_level_res = app_controller.set_time_tagger_channels_trigger_level(
+    SetTriggerLevelParams(
+        time_tagger_network_proxy=tagger,
+        channels_voltage={
+            1: TRIGGER_VOLTAGE,
+            2: TRIGGER_VOLTAGE,
+            3: TRIGGER_VOLTAGE,
+            4: TRIGGER_VOLTAGE,
+        },
+    )
+)
+
+print(set_trigger_level_res, end="\n\n")
+
+serial = req.connected_devices[0].serial_number
+print(serial, "serial number of connceted TT", end="\n\n")
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow(
+    channels=channels,
+    device_serial_number=serial,
+    timetagger_proxy=tagger,
+    app_controller=app_controller,
+    coincidence_virtual_channels=cc_virtual_channel_numbers,
+    measurement_service=time_tagger_measurement_service,
+)
+
+window.show()
+app.exec()
+
+app_controller.close_time_tagger_network_connections(
+    [CloseConnectionPrams(connected_device_name, tagger)]
+)
--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QWidget,  QComboBox, QCheckBox, QGroupBox
 from AppController import AppController
 from ui.graphs.RealTimeGraphsWidget import RealTimeGraphsWidget
 from time_tagger.measurement.service import MeasurementService, CountRateReqParams
@@ -27,13 +28,83 @@ class MainWindow(QMainWindow):
         self.timetagger_proxy = timetagger_proxy
         self.device_serial_number = device_serial_number
         self.measurement_service = measurement_service
-        self._init_graph_widget()
+        #self._init_graph_widget()
+        self._init_interface()
 
-    def _init_graph_widget(self):
+    def _init_interface(self):
+        self.v_left_layout  = QVBoxLayout()
+        v_right_layout = QVBoxLayout()
+        
+        items = ["Single count","Correlation rate","Correlation histogram"]
+        self.selector = QComboBox()
+        self.selector.addItems(items)
+        self.selector.activated.connect(self._show_graph)
+        v_right_layout.addWidget(self.selector)
+
+        self.main_button_list = []
+        box = QGroupBox("Main Channel")
+        box_layout = QVBoxLayout()
+        for channel in range(4): 
+            
+            check = QCheckBox(f"ch {channel+1}")
+            if channel == 0: check.setChecked(True)
+            check.stateChanged.connect(self._show_graph)
+            self.main_button_list += [check]
+            box_layout.addWidget(check)
+        box.setLayout(box_layout)
+        v_right_layout.addWidget(box)
+
+        self.check_button_list = []
+        box = QGroupBox("Second Channel")
+        box_layout = QVBoxLayout()
+        for channel in range(4): 
+            check = QCheckBox(f"ch {channel+1}")
+            if channel == 1: check.setChecked(True)
+            check.stateChanged.connect(self._show_graph)
+            self.check_button_list += [check]
+            box_layout.addWidget(check)
+             
+        box.setLayout(box_layout)
+        v_right_layout.addWidget(box)
+
+
+        self._show_graph()
+        hlayout = QHBoxLayout()
+        self._init_graph_widget((1,2))
+        hlayout.addLayout(self.v_left_layout)
+        hlayout.addLayout(v_right_layout)
+        
+        widget = QWidget()
+        widget.resize(800,600)
+        widget.setLayout(hlayout)
+        self.setCentralWidget(widget) 
+
+
+
+    def _show_graph(self):
+        graph_type = self.selector.currentText()
+        match graph_type:
+            case "Single count":
+                for i,m_channel in enumerate(self.main_button_list):
+                    if m_channel.isChecked():
+                        for j,s_channel in enumerate(self.check_button_list):
+                            if s_channel.isChecked():
+                                self._update_graph_widget((i,j))
+
+            case "Correlation rate":
+                print("test 2")
+            case "Correlation histogram":
+                print("Correlation histogram")
+            case _: assert 0, "Unrechable"
+
+    def _update_graph_widget(self,channels):
+        print(channels)
+
+    def _init_graph_widget(self,channels):
         # Test for the new RealTimeGraphWidget it will be remove
         line_setup = [
             GraphLineSetup(
-                label="ch.1",
+                label=f"ch.",
                 symbol="s",
                 color=Color.RED_PRIMARY,
                 initial_data=[[[0.0], [0]]],
@@ -50,6 +121,6 @@ class MainWindow(QMainWindow):
                                  "1-2",Color.WHITE_PRIMARY,True)
         widget = (coincidence_widget_info_, coincidence_count_params)
         self.graph_widge = RealTimeGraphsWidget([widget],measaurement_service= self.measurement_service)
-        self.setCentralWidget(self.graph_widge) 
+        self.v_left_layout.addWidget(self.graph_widge) 
         
         
