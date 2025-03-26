c43f9d762727aa2eff6a9abc436618c49c1a14d8
You3419 <benamarayou@gmail.com>
2025-03-26 15:40:15 +0100

    add NewBuilder.py

---
+++ src/new_app/Newbuilder.py
@@ -0,0 +1,42 @@
+"""
+
+Build the virtual channels of Time Tagger
+
+"""
+import TimeTagger as TT
+from  new_app.constants import CountRateReqParams, MeasurementType
+
+
+"""
+Build the coincidences channels 
+"""
+
+def build_coincidence_virtual_channel(
+    time_tagger_network_proxy: object,
+    coincidence_channels: list[int],
+    CoinWindow
+):
+    # coincidence_channels_group = list(combinations(single_channels, 2))
+
+    return TT.Coincidences(
+        time_tagger_network_proxy,
+        [coincidence_channels],
+        coincidenceWindow=CoinWindow,
+    )
+
+
+
+"""
+Build the histogram measurment
+Need: CountRateReqParams
+"""
+def build_histogram_measurment(params: CountRateReqParams):
+    measurment_type = params.measurement_type
+    time_tagger_network_proxy = params.time_tagger
+    channels_list = params.channels
+    bin_width = params.bin_width
+    n_bin = params.n_bin
+    match measurment_type:
+        case  MeasurementType.HISTOGRAM_CORR : return TT.Correlation(time_tagger_network_proxy,*channels_list,bin_width, n_bin)
+        case  MeasurementType.HISTOGRAM : return TT.Histogram(time_tagger_network_proxy,*channels_list,bin_width,n_bin)
+        case _: assert 0, "this" + measurment_type.value + "correlation class doesn't exist"
--- src/new_app/constants.py
+++ src/new_app/constants.py
@@ -8,6 +8,19 @@ from enum import Enum
 """
 
 
+#-------- ENU----------
+class Widget_Layout(Enum):
+    SINGLE_COIN = "Single and Coincidence"
+    MEASUREMENT ="Measurement"
+    HISTOGRAM = "Coincidence histogram"
+
+class MeasurementType(Enum):
+    SINGLE_COUNTS = "SINGLE_COUNTS"
+    COINCIDENCES = "COINCIDENCES"
+    HISTOGRAM = "HISTOGRAM"
+    HISTOGRAM_CORR = "HISTOGRAM_CORR"
+
+
 class Color(Enum):
     WHITE_PRIMARY = "#FFFFFF"
     RED_PRIMARY = "#f81b0b"
@@ -16,3 +29,37 @@ class Color(Enum):
     BLACK = "#000000"
 
 Color_List = [Color.BLUE_PRIMARY,Color.GREEN_PRIMARY,Color.RED_PRIMARY,Color.BLACK]
+
+
+
+
+@dataclass
+class GraphLineSetup:
+    label: str
+    symbol: str
+    color: Color
+
+@dataclass
+class WidgetInfo:
+    title : str
+    lines : list[GraphLineSetup]|GraphLineSetup
+    vertical_axis_label : str
+    background_color:Color
+    is_histogram : bool = False
+
+@dataclass
+class Constant:
+    INTEGRATION_TIME = 0.5e12  # IN PICOSECONDS
+    TRIGGER_VOLTAGE = 0.12  # VALUE IN VOLTS
+    SLEEP_TIME = 10e-12
+    GRAPH_ANIMATION_INTERVAL = int(INTEGRATION_TIME * (1e-9))
+
+@dataclass
+class CountRateReqParams:
+    channels: list[int]
+    device_serial: str
+    time_tagger: object
+    measurement_type: MeasurementType
+    histogram_measurement= None
+    bin_width = 50
+    n_bin = 200
