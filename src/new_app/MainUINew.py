fa30ebfe80a4c1ce260be3c3ce5281154ca819fd
You3419 <benamarayou@gmail.com>
2025-03-27 10:21:40 +0100

    init the main interface layout of the app in MainUINew.py

--- src/new_app/MainUINew.py
+++ src/new_app/MainUINew.py
@@ -9,6 +9,9 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import  QMainWindow
 from new_app.constants import Colot_List
 from new_app.NewGraph import RealTimeGraphsWidget
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget
+
+
 
 class MainWindow(QMainWindow):
     def __init__(
@@ -22,5 +25,10 @@ class MainWindow(QMainWindow):
         self.device_serial_number = device_serial_number
         self.timetagger_proxy= timetagger_proxy 
         self.chan_number = chan_number
+        self._init_interface()
     
-    
+    def _init_interface(self):
+        main_layout = QHBoxLayout() 
+        main_widget = QWidget()  
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
